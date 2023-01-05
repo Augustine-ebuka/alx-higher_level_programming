180a2a3b64cfc309cd8e729567e114eeb43ba7da
Augustine-ebuka <augustineebuka98@gmail.com>
2023-01-04 20:39:43 -0800

    first commit

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+tr = "Python is an interpreted, interactive, object-oriented programming\
+
+         language that combines remarkable power with very clear syntax"
+ # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE    
+print(str)
