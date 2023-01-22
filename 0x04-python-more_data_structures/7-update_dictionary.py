7c657ba587b42ceee15aa390044bb8fa66d3bc46
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2023-01-23 00:13:22 +0100

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+
 def update_dictionary(a_dictionary, key, value):
     """
-    function that create dictionary
-    key and value
+    A function that replaces or adds
+    key/value in a dictionary.
     """
     a_dictionary[key] = value
+    return a_dictionary
     return(a_dictionary)
