aa56c635657756121071087d76c0e5fc8fe91fea
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2023-01-23 00:16:14 +0100

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,9 @@
-#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     """
-    function that multiply
-    the value of a diction
-    ry by 2
+    A function that returns a new dictionary
+    with all values multiplied by 2
     """
-     new_dict = {}
-     for key,value in a_dictionary.items():
-         new_dict.update({key:(value*2)})
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict.update({key: (value * 2)})
     return new_dict
