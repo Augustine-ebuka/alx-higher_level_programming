c3d60ee2f9bccf37dfd9ef8a7d95185aa872a2a1
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2023-02-27 13:33:30 +0100

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """this object return the attribute in lkst"""
 def lookup(obj):
     """obj: return the list of attribut of an objrct"""
-    return(dir(obj))`
+    return(dir(obj))
