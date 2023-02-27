8ce784cd918ba1ac7ce1ae1bb4deedc832fb942f
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2023-02-27 13:41:40 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,11 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """this object is about listing and inheritance"""
-class MyList(List)::
+class MyList(list):
+  """list: the list we will sort"""
+  def __init__(self):
+    super().__init__()
+    
+  def print_sorted(self):
+    """the funstion that print sorted list"""
+    print(sorted(self))
 
