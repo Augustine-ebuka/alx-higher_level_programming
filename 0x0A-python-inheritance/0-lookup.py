5a1d4e5df211a13a870c33646ad6f8f945a5d206
Augustine-ebuka <augustineebuka98@gmail.com>
2023-02-27 04:31:40 -0800

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/bin/usr/python3
+"""this object return the attribute in lkst"""
+def lookup(obj):
+    """obj: return the list of attribut of an objrct"""
+    return(dir(obj))`
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,4 @@
+#!/bin/usr/python3
+"""this object is about listing and inheritance"""
+class MyList(List)::
+
