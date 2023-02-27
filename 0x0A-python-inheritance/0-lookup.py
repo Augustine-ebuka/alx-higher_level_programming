9db4629653cbd4a85eff978c03af07af487b085e
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2023-02-27 14:06:46 +0100

    first commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""this object return the attribute in lkst"""
+"""Defines an object attribute lookup function."""
+
+
 def lookup(obj):
-    """obj: return the list of attribut of an objrct"""
-    return(dir(obj))
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""this object is about listing and inheritance"""
-class MyList(list):
-  """list: the list we will sort"""
-  def __init__(self):
-    super().__init__()
-    
-  def print_sorted(self):
-    """the funstion that print sorted list"""
-    print(sorted(self))
+"""
+contains the MyList class
+"""
 
+
+class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
