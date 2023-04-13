670125d8bbb70c88492f62b6257eaead0dd44847
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2023-04-13 18:45:37 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const argv = require('process');
-if (argv[2] === undefined) {
-    console.log('not found');
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-    console.log(argv[2]);
+  console.log(process.argv[2]);
 }
