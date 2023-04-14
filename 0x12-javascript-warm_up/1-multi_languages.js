1a9fee63e2afa8ebcae84464053a94a128d9c8e9
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2023-04-14 05:03:11 +0100

    added all the remaining task

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const line1 = 'C is fun';
-const line2 = 'JavaScript is amazing';
-const line3 = 'Python is cool';
-console.log(line1);
-console.log(line3);
-console.log(line2);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
