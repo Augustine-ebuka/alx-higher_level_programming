4528297fd54fa3f3b1c1837794ef459700436b88
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2023-04-28 01:28:52 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL
-if [ $# -eq 0 ]; then
-  echo "Error: URL not provided"
-  exit 1
-fi
-
-# Send a GET request to the URL and store the response body in a variable
-response=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-
-# Display the size of the response body in bytes
-echo $response
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
