b35816002875e2e2fc3d6167ecd8c5a56b8859e4
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2023-07-03 14:48:49 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,25 +7,26 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new Square.
+        """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
         """
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
-        """getter method """
-        return(self.__size)
-    @size.stter
-    def size(self,value):
-        """this is the stter"""
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif(value < 0):
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size =value 
+        self.__size = value
 
     def area(self):
-        """ return the area of the square """
-        return(self.__size * self.__size)
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
